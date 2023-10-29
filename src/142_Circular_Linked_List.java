87cb0fcebfa1a21efbf25910830515ebd4a350f6
Alvis Wang <88550789+AlvisWhy@users.noreply.github.com>
2023-10-29 15:04:51 -0400

    Create 142_Circular_Linked_List.java

---
+++ src/142_Circular_Linked_List.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next!= null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                ListNode index1 = head;
+                ListNode index2 = fast;
+                while (index1 != index2) {
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+
+        return null;
+    }
+}
