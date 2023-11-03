7daf08dda7a04bfd8afe6bb5e1d36685df46f407
Alvis Wang <88550789+AlvisWhy@users.noreply.github.com>
2023-11-03 02:11:00 -0400

    Update and rename 255_Realize_Queue_with_Stack.java to 232_Realize_Queue_with_Stack.java

---
+++ src/232_Realize_Queue_with_Stack.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+class MyQueue {
+   
+    private Stack<Integer> stackIn;
+    private Stack<Integer> stackOut;
+
+    public MyQueue() {
+        stackIn = new Stack<Integer>();
+        stackOut = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        stackIn.push(x);
+    }
+    
+    public int pop() {
+        if (stackOut.isEmpty()) {
+            while(!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        }
+
+        return stackOut.pop();
+    }
+    
+    public int peek() {
+        if (stackOut.isEmpty()) {
+            while(!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+        }
+        return stackOut.peek();
+    }
+    
+    public boolean empty() {
+        return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+
+}
