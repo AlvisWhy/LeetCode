18b206042fe752b5b9a91cd0c205148ab522ab0e
Alvis Wang <88550789+AlvisWhy@users.noreply.github.com>
2023-11-10 01:54:27 -0500

    Create 104_Maximum_depth_of_binary_tree.java

---
+++ src/104_Maximum_depth_of_binary_tree.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue =  new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        int maxLayer = 0;
+        while(!queue.isEmpty()) {
+            maxLayer += 1;
+            List<Integer> temp = new ArrayList<Integer>();
+            int len = queue.size();
+            while(len > 0) {
+                TreeNode tempNode = queue.poll();
+                temp.add(tempNode.val);
+                if (tempNode.left != null) {
+                    queue.offer(tempNode.left);
+                }
+                if (tempNode.right != null) {
+                    queue.offer(tempNode.right);
+                }
+                len --;
+            } 
+        }
+        return maxLayer;
+    }
+}
