af991651baa3af469525d9827c07368c751a7c74
Alvis Wang <88550789+AlvisWhy@users.noreply.github.com>
2023-11-10 01:55:13 -0500

    Create 117_Fill_the_next_right_node_pointer_of_each_node2.java

---
+++ src/117_Fill_the_next_right_node_pointer_of_each_node2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) {
+            return root;
+        }
+        Queue<Node> queue =  new ArrayDeque<Node>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+            while(len > 0) {
+                Node tempNode = queue.poll();
+                if (len>1) {
+                    tempNode.next = queue.peek();
+                }
+                if (tempNode.left != null) {
+                    queue.offer(tempNode.left);
+                }
+                if (tempNode.right != null) {
+                    queue.offer(tempNode.right);
+                }
+                len --;
+            }
+        }
+        return root;
+    }
+}
