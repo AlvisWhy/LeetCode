6fe78a5084ec4232b0d3b6c634c9a89f74f56e58
Alvis Wang <88550789+AlvisWhy@users.noreply.github.com>
2023-11-11 00:20:53 -0500

    Update 104_Maximum_depth_of_binary_tree.java

--- src/104_Maximum_depth_of_binary_tree.java
+++ src/104_Maximum_depth_of_binary_tree.java
@@ -24,4 +24,12 @@ class Solution {
         }
         return maxLayer;
     }
+
+    // recursion
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return (1 + Math.max(maxDepth(root.left), maxDepth(root.right)));
+    }
 }
