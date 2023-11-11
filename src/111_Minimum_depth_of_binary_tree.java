9d3fe54aa18e387adb4e54a164505965d807163a
Alvis Wang <88550789+AlvisWhy@users.noreply.github.com>
2023-11-11 00:32:30 -0500

    Update 111_Minimum_depth_of_binary_tree.java

--- src/111_Minimum_depth_of_binary_tree.java
+++ src/111_Minimum_depth_of_binary_tree.java
@@ -27,4 +27,23 @@ class Solution {
         }
         return 0;
     }
+
+    // recursion
+        public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        if (root.right==null && root.left == null) {
+            return 1;
+        }
+        else if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        else if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+        else {
+            return 1 + Math.min(minDepth(root.right), minDepth(root.left));
+        }
+    }
 }
